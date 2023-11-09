41a359851d9e2e4f926d06fa89384bfe317364e5
Joe Bailey <joe@prioritydigitalhealth.com>
2023-11-09 16:26:47 +0000

    Add eslint and other project files

--- rollup.config.js
+++ rollup.config.js
@@ -7,58 +7,58 @@ import serve from 'rollup-plugin-serve-proxy'
 const production = !process.env.ROLLUP_WATCH
 
 export default {
-	input: 'src/main.js',
-	output: {
-		sourcemap: true,
-		format: 'iife',
-		name: 'app',
-		file: 'public/build/bundle.js'
-	},
-	plugins: [
-		!production && serve({
-			// Launch in browser (default: false)
-			open: true,
+  input: 'src/main.js',
+  output: {
+    sourcemap: true,
+    format: 'iife',
+    name: 'app',
+    file: 'public/build/bundle.js'
+  },
+  plugins: [
+    !production && serve({
+      // Launch in browser (default: false)
+      open: true,
 
-			// Show server address in console (default: true)
-			verbose: false,
+      // Show server address in console (default: true)
+      verbose: false,
 
-			// Folder to serve files from
-			contentBase: 'public',
+      // Folder to serve files from
+      contentBase: 'public',
 
-			// Options used in setting up server
-			host: 'localhost',
-			port: 8999,
+      // Options used in setting up server
+      host: 'localhost',
+      port: 8999,
 
-			//set headers
-			headers: {
-				'Access-Control-Allow-Origin': '*'
-			}
-		}),
-		svelte({
-			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file - better for performance
-			css: css => {
-				css.write('public/build/bundle.css');
-			}
-		}),
+      // set headers
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    }),
+    svelte({
+      dev: !production,
+      // we'll extract any component CSS out into
+      // a separate file - better for performance
+      css: (css) => {
+        css.write('public/build/bundle.css')
+      }
+    }),
 
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration -
-		// consult the documentation for details:
-		// https://github.com/rollup/plugins/tree/master/packages/commonjs
-		resolve({
-			browser: true,
-			dedupe: ['svelte']
-		}),
-		commonjs(),
+    // If you have external dependencies installed from
+    // npm, you'll most likely need these plugins. In
+    // some cases you'll need additional configuration -
+    // consult the documentation for details:
+    // https://github.com/rollup/plugins/tree/master/packages/commonjs
+    resolve({
+      browser: true,
+      dedupe: ['svelte']
+    }),
+    commonjs(),
 
-		// If we're building for production (npm run build
-		// instead of npm run dev), minify
-			production && terser()
-	],
-	watch: {
-		clearScreen: false
-	}
+    // If we're building for production (npm run build
+    // instead of npm run dev), minify
+    production && terser()
+  ],
+  watch: {
+    clearScreen: false
+  }
 }
