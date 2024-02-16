c3d238062b48df683b377ab18187b9845f30e266
Joe <joe@joebailey.xyz>
2024-02-16 17:58:59 +0000

    Tweak categories

--- server/api/shows.get.ts
+++ server/api/shows.get.ts
@@ -1,5 +1,5 @@
 import type { H3Event } from 'h3'
-import { asc, eq, inArray, and, countDistinct, gt, sql, notInArray, gte, isNull, isNotNull, or } from 'drizzle-orm'
+import { asc, eq, inArray, and, countDistinct, gt, sql, notInArray, lte } from 'drizzle-orm'
 import { SQLiteSelect } from 'drizzle-orm/sqlite-core'
 import { getAuthenticatedUserEmail } from '../lib/auth'
 import { tvShows, users, episodateTvShows, episodes, watchedEpisodes } from '../../db/schema'
@@ -29,20 +29,19 @@ export default defineEventHandler(async (event: H3Event): Promise<CustomSearch> 
 
   function querySetup<T extends SQLiteSelect> (qb: T) {
     return qb.leftJoin(
-      countdown,
-      eq(episodateTvShows.id, countdown.showId)
+      tvShows,
+      eq(tvShows.showId, episodateTvShows.id)
     )
-      .leftJoin(
-        tvShows,
-        eq(tvShows.showId, episodateTvShows.id)
-      )
       .leftJoin(
         users,
         eq(users.id, tvShows.userId)
       )
       .leftJoin(
         episodes,
-        eq(episodes.episodateTvShowId, episodateTvShows.id)
+        and(
+          eq(episodes.episodateTvShowId, episodateTvShows.id),
+          lte(sql`date(${episodes.air_date})`, sql`date('now')`)
+        )
       )
       .leftJoin(
         watchedEpisodes,
@@ -58,63 +57,52 @@ export default defineEventHandler(async (event: H3Event): Promise<CustomSearch> 
 
   function categoryCancelled<T extends SQLiteSelect> (qb: T) {
     return qb.where(
+      // Show has status of cancelled
       inArray(sql`lower(${episodateTvShows.status})`, ['canceled-ended', 'ended'])
     )
-      .having(({ watchedEpisodeCount, episodeCount }) =>
-        eq(watchedEpisodeCount, episodeCount)
+      .having(({ watchedEpisodeCount, pastEpisodeCount }) =>
+        // You have watched all released episodes
+        eq(watchedEpisodeCount, pastEpisodeCount)
       )
   }
 
   function categoryWantToWatch<T extends SQLiteSelect> (qb: T) {
     return qb.having(({ watchedEpisodeCount }) =>
+      // You have not watched any episodes
       eq(watchedEpisodeCount, 0)
     )
   }
 
   function categoryToCatchUpOn<T extends SQLiteSelect> (qb: T) {
-    return qb.where(
-      isNull(countdown.showId)
-    )
-      .having(({ watchedEpisodeCount, episodeCount }) =>
-        and(
-          gt(sql`${episodeCount} - 1`, watchedEpisodeCount),
-          gt(watchedEpisodeCount, 0)
-        )
+    return qb.having(({ watchedEpisodeCount, pastEpisodeCount }) =>
+      and(
+        // There have been episodes released that you have not watched
+        gt(pastEpisodeCount, watchedEpisodeCount),
+        // You have watched at least one episode
+        gt(watchedEpisodeCount, 0)
       )
+    )
   }
 
   function categoryWaitingFor<T extends SQLiteSelect> (qb: T) {
     return qb.where(
+      // Show does not have status of cancelled
       notInArray(sql`lower(${episodateTvShows.status})`, ['canceled-ended', 'ended'])
     )
-      .having(({ watchedEpisodeCount, episodeCount }) =>
-        or(
-          gte(watchedEpisodeCount, sql`${episodeCount} - 1`),
-          isNotNull(countdown.showId)
-        )
+      .having(({ watchedEpisodeCount, pastEpisodeCount }) =>
+        // You have watched all released episodes
+        eq(pastEpisodeCount, watchedEpisodeCount)
       )
   }
 
   const DB = await useDb(event)
 
-  const countdown = DB.selectDistinct({
-    showId: sql<number>`${episodes.episodateTvShowId}`.as('countdownShowId')
-  })
-    .from(episodes)
-    .where(
-      gt(sql`date(${episodes.air_date})`, sql`date('now')`)
-    )
-    .orderBy(
-      asc(sql`date(${episodes.air_date})`)
-    )
-    .as('countdown')
-
   // If this pagination strategy starts to be slow
   // We should instead return limit + 1
   // From this we can work out that there are more records to return
   const countQuery = DB.select({
-    episodeCount: countDistinct(episodes.id),
-    watchedEpisodeCount: countDistinct(watchedEpisodes.id)
+    watchedEpisodeCount: countDistinct(watchedEpisodes.id),
+    pastEpisodeCount: countDistinct(episodes.id)
   })
     .from(episodateTvShows)
     .$dynamic()
@@ -130,8 +118,8 @@ export default defineEventHandler(async (event: H3Event): Promise<CustomSearch> 
     status: episodateTvShows.status,
     image_thumbnail_path: episodateTvShows.image_thumbnail_path,
     updatedAt: episodateTvShows.updatedAt,
-    episodeCount: countDistinct(episodes.id),
-    watchedEpisodeCount: countDistinct(watchedEpisodes.id)
+    watchedEpisodeCount: countDistinct(watchedEpisodes.id),
+    pastEpisodeCount: countDistinct(episodes.id)
   })
     .from(episodateTvShows)
     .$dynamic()
