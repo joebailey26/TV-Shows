b9c324d9c4894eb5cae44065b4f0f2003f7eeb46
Joe Bailey <joe@joebailey.xyz>
2025-06-07 23:29:55 +0100

    fix stylelint plugin path

---
+++ packages/stylelint-config-galexia/plugin/index.js
@@ -0,0 +1,40 @@
+const stylelint = require('stylelint');
+const path = require('node:path');
+const propertiesOrderRule = require(
+  path.join(
+    path.dirname(require.resolve('stylelint-order')),
+    'rules/properties-order/index.js'
+  )
+).rule;
+const configCreator = require('../config/configCreator');
+
+const ruleName = 'plugin/rational-order';
+
+module.exports = stylelint.createPlugin(
+  ruleName,
+  (enabled, options, context) => (postcssRoot, postcssResult) => {
+    const validOptions = stylelint.utils.validateOptions(
+      postcssResult,
+      ruleName,
+      {
+        actual: enabled,
+        possible: [true, false],
+      },
+      {
+        actual: options,
+        optional: true,
+        possible: {
+          'border-in-box-model': [true, false],
+          'empty-line-between-groups': [true, false],
+        },
+      },
+    );
+    if (!enabled || !validOptions) {
+      return;
+    }
+    const expectation = configCreator(options);
+    propertiesOrderRule(expectation, undefined, context)(postcssRoot, postcssResult);
+  },
+);
+
+module.exports.ruleName = ruleName;
