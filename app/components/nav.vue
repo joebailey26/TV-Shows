90d03ad2ac07c77ba0f617a418a3e025c7194c1e
Joe Bailey <joe@joebailey.xyz>
2025-06-07 12:12:51 +0100

    feat(nav): add responsive menu

--- app/components/nav.vue
+++ app/components/nav.vue
@@ -18,6 +18,43 @@ nav {
   align-items: center;
   justify-content: space-between
 }
+.menu {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%
+}
+.mobile-toggle {
+  display: none;
+  color: inherit;
+  font-size: 1.5rem;
+  background: none;
+  border: none;
+  cursor: pointer
+}
+
+@media (max-width: 768px) {
+  .mobile-toggle {
+    display: block
+  }
+  .menu {
+    display: none;
+    flex-direction: column;
+    align-items: stretch;
+    width: 100%;
+    &.open {
+      display: flex
+    }
+  }
+  .left,
+  .right {
+    flex-direction: column;
+    align-items: stretch;
+    width: 100%
+  }
+}
 .nav-link {
   font-size: 1.1rem;
   text-decoration: none
@@ -30,31 +67,36 @@ nav {
 <template>
   <nav>
     <div class="inner-content">
-      <div class="left">
-        <nuxt-link to="/my-shows" class="nav-link">
-          All Shows
-        </nuxt-link>
-        <nuxt-link to="/my-shows?category=toCatchUpOn" class="nav-link">
-          To Catch Up On
-        </nuxt-link>
-        <nuxt-link to="/my-shows?category=wantToWatch" class="nav-link">
-          Want To Watch
-        </nuxt-link>
-        <nuxt-link to="/my-shows?category=waitingFor" class="nav-link">
-          Waiting For
-        </nuxt-link>
-        <nuxt-link to="/my-shows?category=cancelled" class="nav-link">
-          Cancelled
-        </nuxt-link>
-      </div>
-      <div class="right">
-        <Search />
-        <a class="download-calendar button" :href="`/api/calendar/${userEmail}`" :download="`tv_joebailey_xyz_calendar_${userEmail}`">
-          Download calendar
-        </a>
-        <a class="sign-out button" href="/api/auth/signout">
-          Sign Out
-        </a>
+      <button class="mobile-toggle" type="button" @click="isMenuOpen = !isMenuOpen">
+        ☰
+      </button>
+      <div :class="['menu', { open: isMenuOpen }]">
+        <div class="left">
+          <nuxt-link to="/my-shows" class="nav-link">
+            All Shows
+          </nuxt-link>
+          <nuxt-link to="/my-shows?category=toCatchUpOn" class="nav-link">
+            To Catch Up On
+          </nuxt-link>
+          <nuxt-link to="/my-shows?category=wantToWatch" class="nav-link">
+            Want To Watch
+          </nuxt-link>
+          <nuxt-link to="/my-shows?category=waitingFor" class="nav-link">
+            Waiting For
+          </nuxt-link>
+          <nuxt-link to="/my-shows?category=cancelled" class="nav-link">
+            Cancelled
+          </nuxt-link>
+        </div>
+        <div class="right">
+          <Search />
+          <a class="download-calendar button" :href="`/api/calendar/${userEmail}`" :download="`tv_joebailey_xyz_calendar_${userEmail}`">
+            Download calendar
+          </a>
+          <a class="sign-out button" href="/api/auth/signout">
+            Sign Out
+          </a>
+        </div>
       </div>
     </div>
   </nav>
@@ -68,7 +110,8 @@ export default defineComponent({
     const { user } = useAuth()
 
     const state = reactive({
-      userEmail: ''
+      userEmail: '',
+      isMenuOpen: false
     })
 
     if (user.value?.email) {
