ad9c61155aababc49588e3424811fa1e6ab69d3b
Joe Bailey <joe@prioritydigitalhealth.com>
2026-05-19 21:57:24 +0100

    Use not exists for new migration

--- drizzle/0002_watch_partners.sql
+++ drizzle/0002_watch_partners.sql
@@ -1,19 +1,19 @@
-CREATE TABLE `watchPartners` (
+CREATE TABLE IF NOT EXISTS `watchPartners` (
   `id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
   `userId` text NOT NULL,
   `name` text NOT NULL,
   FOREIGN KEY (`userId`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE cascade
 );
 
-CREATE INDEX `watchPartnersUserIdIdx` ON `watchPartners` (`userId`);
+CREATE INDEX IF NOT EXISTS `watchPartnersUserIdIdx` ON `watchPartners` (`userId`);
 
-CREATE UNIQUE INDEX `watchPartnersUserIdNameIdx` ON `watchPartners` (`userId`, `name` COLLATE NOCASE);
+CREATE UNIQUE INDEX IF NOT EXISTS `watchPartnersUserIdNameIdx` ON `watchPartners` (`userId`, `name` COLLATE NOCASE);
 
-CREATE TABLE `showWatchPartners` (
+CREATE TABLE IF NOT EXISTS `showWatchPartners` (
   `showId` integer NOT NULL,
   `watchPartnerId` integer NOT NULL,
   FOREIGN KEY (`showId`) REFERENCES `tv_shows`(`id`) ON UPDATE no action ON DELETE cascade,
   FOREIGN KEY (`watchPartnerId`) REFERENCES `watchPartners`(`id`) ON UPDATE no action ON DELETE cascade
 );
 
-CREATE UNIQUE INDEX `showPartnerIdx` ON `showWatchPartners` (`showId`,`watchPartnerId`);
+CREATE UNIQUE INDEX IF NOT EXISTS `showPartnerIdx` ON `showWatchPartners` (`showId`,`watchPartnerId`);
